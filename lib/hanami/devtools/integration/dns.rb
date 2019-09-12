a3cb14a330656b7e494d1efc741eb0c3f4ccdb5e
Luca Guidi <me@lucaguidi.com>
2019-09-12 19:44:27 +0200

    Helper to stub DNS lookup

---
+++ lib/hanami/devtools/integration/dns.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "resolv-replace"
+
+module RSpec
+  module Support
+    # DNS stub utilities
+    module Dns
+      private
+
+      def stub_dns_hosts(hosts)
+        file = Pathname.new(Dir.pwd).join("tmp", "hosts-#{SecureRandom.uuid}")
+        write(file, hosts)
+
+        hosts_resolver = Resolv::Hosts.new(file.to_s)
+        dns_resolver = Resolv::DNS.new
+
+        Resolv::DefaultResolver.replace_resolvers([hosts_resolver, dns_resolver])
+      end
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.include RSpec::Support::Dns, type: :integration
+end
