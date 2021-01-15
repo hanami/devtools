7e5534598e0373e3669f91000465d4d575b94763
Luca Guidi <me@lucaguidi.com>
2021-01-15 17:34:21 +0100

    Ignore Capybara/Poltergeist JavaScript errors (#63)

--- lib/hanami/devtools/integration/capybara.rb
+++ lib/hanami/devtools/integration/capybara.rb
@@ -4,14 +4,17 @@ require "capybara"
 require "capybara/rspec"
 require "capybara/dsl"
 require "hanami/devtools/integration/platform"
+require "capybara/poltergeist"
 
 RSpec.configure do |config|
   config.include Capybara::DSL, type: :integration
 end
 
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, {js_errors: false})
+end
+
 Capybara.configure do |config|
   config.run_server = false
-
-  require "capybara/poltergeist"
   config.default_driver = :poltergeist
 end
